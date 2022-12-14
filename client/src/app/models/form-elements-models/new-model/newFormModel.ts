5ab2d5ad5149ceec31c469c43fbc3f2bd650b331
Tomi Nikolov <tomi.v.nikolov@gmail.com>
2022-12-14 19:56:17 +0200

    clear newFormModel

--- client/src/app/models/form-elements-models/new-model/newFormModel.ts
+++ client/src/app/models/form-elements-models/new-model/newFormModel.ts
@@ -1,16 +1,12 @@
 import { Validators } from "@angular/forms";
 import { NOT_VALID_CHARACTERS, SPECIAL_CHARACTERS_ARE_NOT_ALLOOWED } from "src/app/utils/const";
 import { patternError } from "../utils/errorsMessages";
-import { FormFactoryFunction } from "../utils/form-element-factory";
+import { FormElementFactoryFunction } from "../utils/form-element-factory";
 
-export const newFormModel = FormFactoryFunction
+export const newFormModel = FormElementFactoryFunction
   ('', '', '', 0, '',
     ['',
-      [ 
-        Validators.pattern(NOT_VALID_CHARACTERS) 
-      ]
+      [ ]
     ],
-    [
-      patternError( SPECIAL_CHARACTERS_ARE_NOT_ALLOOWED )
-    ]
+    [ ]
   );
